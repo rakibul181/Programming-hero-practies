f3e9bd55bb6724f26c394ef23a58a446fcc33e39
rakibul181 <rakibulhasan0641@gmail.com>
2022-08-09 12:35:45 +0600

    scarch explore

---
+++ Javascript-m21/scarch copy.js
@@ -0,0 +1,3 @@
+const lirics = "Tumi bondhu kalA pakhi ami jen ki, bosonto kale tomay bolte pari no, sada sada Kala kalA rong jomese sda kalA"
+
+const existKala = lirics.
---
+++ Javascript-m21/scarch.js
@@ -0,0 +1,6 @@
+const lirics = "Tumi bondhu kalA pakhi ami jen ki, bosonto kale tomay bolte pari no, sada sada Kala kalA rong jomese sda kalA"
+
+const existKala = lirics.toLowerCase().includes('kAla'.toLowerCase())
+console.log(existKala)
+const existsada = lirics.includes('sada')
+console.log(existsada)
