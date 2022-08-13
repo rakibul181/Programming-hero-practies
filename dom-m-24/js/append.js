8ebb040b9781a870af1585805cada7515846ad89
rakibul181 <rakibulhasan0641@gmail.com>
2022-08-14 03:17:21 +0600

    We are ready to append something by Javascript in HTMl Doc

---
+++ dom-m-24/js/append.js
@@ -0,0 +1,5 @@
+const myPlascs = document.getElementById('plases')
+const liadd = document.createElement('p')
+liadd.innerText = 'Ami sob jaygay jete cai but amar Taka nai'
+
+myPlascs.append(liadd)
