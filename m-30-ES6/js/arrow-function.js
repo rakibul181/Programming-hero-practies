d54dd7aae6a49fdbeb5938c5e55fed6bb190ae77
rakibul181 <rakibulhasan0641@gmail.com>
2022-08-29 20:53:41 +0600

    arrow function explored

---
+++ m-30-ES6/js/arrow-function.js
@@ -0,0 +1,18 @@
+const testArrow =  (para) => para* 5
+
+// console.log(testArrow(6))
+
+const myFunc = () =>{
+    return 5+6
+}
+
+// console.log(myFunc())
+
+const arry = [ 1, 4, 2, 3, 5, 6, 7, 8, 9, 10]
+// const sum = ...arry
+
+console.log(...arry)
+
+
+const obj =  {...arry}
+console.log(obj)
