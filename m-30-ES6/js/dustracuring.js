c04679f5d670fdb86f164ce779dee87bc2998f77
rakibul181 <rakibulhasan0641@gmail.com>
2022-08-29 21:07:06 +0600

    Arry and obj destructuring

---
+++ m-30-ES6/js/dustracuring.js
@@ -0,0 +1,25 @@
+const [ab , , ...other] =[{
+    name: 'rakib',
+    id: 2011122181,
+    dept: "Physics",
+    sesion: "2019-20"
+},
+{
+    name: 'akib',
+    id: 2011122181,
+    dept: "Physics",
+    sesion: "2019-20"
+},{
+    name: 'sakib',
+    id: 2011122181,
+    dept: "Physics",
+    sesion: "2019-20"
+},{
+    name: 'nakib',
+    id: 2011122181,
+    dept: "Physics",
+    sesion: "2019-20"
+}]
+
+console.log(ab)
+console.log(other)
