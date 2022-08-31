ef71d19bd3f09d006086cefa1794935fb1434fa3
rakibul181 <rakibulhasan0641@gmail.com>
2022-08-31 21:56:32 +0600

    JSON Explored

---
+++ Api-Explore/js/js.js
@@ -0,0 +1,22 @@
+const user ={name:'Rakibul Hasan', id:2011122181, Dept:'Physics'}
+
+const jsonUser = JSON.stringify(user)
+
+console.log(user)
+console.log(jsonUser)
+
+
+const shop ={
+    owner:"adhara",
+    address:{
+        street:'suger Mill',
+        road:'Jogoti', 
+        city: 'Kushtia',
+        Country: 'Bangladesh'
+    },
+    product:['alo', 'Komra', 'Law', ' Ful gach'],
+    isOpen: true,
+    revenew: 1000,
+    isRegstraed: true
+    
+}
