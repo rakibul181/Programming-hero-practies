2ff7e296378ddaf100390c8df34a3631aae4dca0
rakibul181 <rakibulhasan0641@gmail.com>
2022-09-02 16:51:29 +0600

    daynamic api colled from Meal DB

--- more-API/js/meal.js
+++ more-API/js/meal.js
@@ -8,13 +8,13 @@ const displayMeals = (data) =>{
     const mealsContainer = document.getElementById('meals-container')
     mealsContainer.innerText = ""
     data.forEach(meal => {
-        console.log(meal)
+        // console.log(meal)
         // mealDiv.innerHTM = ``
         const mealDiv = document.createElement('div')
         
         mealDiv.classList.add('col')
         mealDiv.innerHTML =`
-        <div class="card h-100">
+        <div onclick="mealInfo(${meal.idMeal})" class="card h-100">
             <img src="${meal.strMealThumb}" class="card-img-top" alt="...">
             <div class="card-body">
                 <h5 class="card-title">${meal.strMeal}</h5>
@@ -25,6 +25,31 @@ const displayMeals = (data) =>{
         mealsContainer.appendChild(mealDiv)
 
     });
+}
+const mealInfo = (idMeal) => {
+    const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${idMeal}`
+    // console.log(url)
+    fetch(url)
+    .then(res => res.json())
+    .then(data => displayMealInfo(data.meals[0]))
+}
+
+const displayMealInfo = (meal) =>{
+    console.log(meal)
+
+    const displayContainer = document.getElementById('displayMealinfo')
+    displayContainer.innerHTML = `` 
+    const displayDiv = document.createElement('div')
+    displayDiv.innerHTML =`
+    <div class='mb-4'>
+    <img src="${meal.strMealThumb}" class="card-img-top" alt="...">
+    <div class="card-body">
+        <h3>${meal.strMeal}</h3>
+        <p class="card-text">${meal.strInstructions}</p>
+    </div>
+    </div>
+    `
+    displayContainer.appendChild(displayDiv)
 }
 
 const mealSearch =()=>{
